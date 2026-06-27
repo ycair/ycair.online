9254b1d076fae9491b48543a109c7cf67399b2a3
YCAIR <ycair.support@gmail.com>
2026-06-28 01:17:56 +0800

    fix: add let _ = to embed_resource::compile

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     tauri_build::build();
     #[cfg(target_os = "windows")]
-    embed_resource::compile("app.rc", embed_resource::NONE);
+    let _ = embed_resource::compile("app.rc", embed_resource::NONE);
 }
